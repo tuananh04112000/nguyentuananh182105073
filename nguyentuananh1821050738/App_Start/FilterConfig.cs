d14b4c34f5f4b44b0c731ada6f7b9b8600ee4ee9
ANH NGUYEN TUAN <1821050738@student.humg.edu.vn>
2021-08-31 12:19:40 +0700

    Add project files.

---
+++ nguyentuananh1821050738/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace nguyentuananh1821050738
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
---
+++ nguyentuananh1821050738/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="nguyentuananh1821050738.MvcApplication" Language="C#" %>
